ad0eb222bd5e2196d7430e7e08579eff103508de
ZeeksX <155986996+ZeeksX@users.noreply.github.com>
2024-02-06 16:44:59 +0100

    Update test1.js

--- test1.js
+++ test1.js
@@ -4,18 +4,21 @@ document.addEventListener('DOMContentLoaded', function(){
     const emailInput = document.querySelector('#email');
     const emailError = document.getElementById("emailError");
     const successMessage = document.getElementById("successMessage");
+    const button = document.querySelector('.btn')
     
     // formula to check validity of email
     function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         return emailRegex.test(email);
     }
     //When the form submits
     signupForm.addEventListener('submit', function(event){
         if(!isValidEmail(emailInput.value)){
             event.preventDefault();
-            emailError.style.display='flex'
-            emailError.textContent = 'Valid email address required'
+            emailError.style.display='flex';
+            emailError.textContent = 'Valid email address required';
+            emailInput.style.color='#fe8376';
+            emailInput.style.backgroundColor='#ffe8e6';
             emailInput.classList.add('invalid-input'); 
         } else {
             emailError.textContent = '';
@@ -24,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function(){
             event.preventDefault();
         }
     });
+    
     // function to display success message
     function showSuccessMessage(){
         document.getElementById('half').innerText += emailInput.value;
